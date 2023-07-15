365e97b6cbc4ad89e0c2234aeaf0ee34ddee5445
Nishant Bansal <nishant.bansal.mec21@iitbhu.ac.in>
2023-07-15 12:42:56 +0530

    Responsive

--- src/components/Footer/Code/footer.js
+++ src/components/Footer/Code/footer.js
@@ -5,120 +5,104 @@ import "../Style/footer.css";
 function SimpleFooter() {
   return (
     <footer className="footer-section">
-    <div className="container">
-      <div className="footer-cta pt-5">
-        <div className="row">
-          <div className="col-xl-4 col-md-4 mb-30">
-            <div className="single-cta">
-              <i className="fas fa-map-marker-alt"></i>
-              <div className="cta-text">
-                <h4>Find us</h4>
-                <span>Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016</span>
+      <div className="container">
+        <div className="footer-cta pt-5">
+          <div className="row">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="cta-text">
+                  <h4>Find us</h4>
+                  <span>IIT(BHU), VARANASI </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-phone"></i>
+                <div className="cta-text">
+                  <h4>Call us</h4>
+                  <span>+91 12345 67891</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="far fa-envelope-open"></i>
+                <div className="cta-text">
+                  <h4>Mail us</h4>
+                  <span>brokerax@gmail.com</span>
+                </div>
               </div>
             </div>
           </div>
-          <div className="col-xl-4 col-md-4 mb-30">
-            <div className="single-cta">
-              <i className="fas fa-phone"></i>
-              <div className="cta-text">
-                <h4>Call us</h4>
-                <span>+91 1234567890</span>
+        </div>
+        <div className="footer-content pt-5 pb-1">
+          <div className="row">
+            <div className="col-xl-4 col-lg-4 mb-50">
+              <div className="footer-widget">
+                <div className="footer-social-icon">
+                  <span>Follow us</span>
+                  <ul className="social_icon">
+                    <li>
+                      <a href="https://www.facebook.com">
+                        <i className="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.twitter.com">
+                        <i className="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.instagram.com">
+                        <i className="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.youtube.com">
+                        <i className="fab fa-youtube"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-xl-4 col-md-4 mb-30">
-            <div className="single-cta">
-              <i className="far fa-envelope-open"></i>
-              <div className="cta-text">
-                <h4>Mail us</h4>
-                <span>contact@gmail.com</span>
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Useful Links</h3>
+                </div>
+                <ul>
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a href="AboutUs">About Us</a>
+                  </li>
+                  <li>
+                    <a href="Markets">Markets</a>
+                  </li>
+                  <li>
+                    <a href="RenderingDash">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="AboutUs">Our Team</a>
+                  </li>
+                  <li>
+                    <a href="ContactUs">Contact us</a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="footer-content pt-5 pb-1">
-        <div className="row">
-          <div className="col-xl-4 col-lg-4 mb-50">
-            <div className="footer-widget">
-              <div className="footer-logo">
-                <a href="index.html"><img src="assets/images/logo.png" className="img-fluid" alt="logo" /></a>
-              </div>
-              <div className="footer-text">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-              </div>
-              <div className="footer-social-icon">
-                <span>Follow us</span>
-                <ul className="social_icon">
-                  <li><a href="#"><i className="fa-brands fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i className="fa-brands fa-twitter"></i></a></li>
-                  <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
-                  <li><a href="#"><i className="fa-brands fa-youtube"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-            <div className="footer-widget">
-              <div className="footer-widget-heading">
-                <h3>Useful Links</h3>
-              </div>
-              <ul>
-              <li><a href="/">Home</a></li>
-                <li><a href="#">Our Team</a></li>
-                <li><a href="AboutUs">About Us</a></li>
-                <li><a href="Markets">Markets</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="RenderingDash">Dashboard</a></li>
-                <li><a href="#">Our Policies</a></li>
-                <li><a href="#">Our Team</a></li>
-                <li><a href="ContactUs">Contact us</a></li>
-                </ul>
-                          </div>
-                      </div>
-
-                      <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-            <div className="footer-widget">
-              <div className="footer-widget-heading">
-                <h3>Subscribe</h3>
-              </div>
-              <div className="footer-text mb-25">
-                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
-              </div>
-              <div className="subscribe-form">
-                <form action="#">
-                  <input type="text" placeholder="Email Address" />
-                  <button><i className="fab fa-telegram-plane"></i></button>
-                </form>
-              </div>
-            </div>
-          </div>
-          </div>
-            </div>
-          </div>
-          <div class="copyright-area">
-              <div class="container">
-                  <div class="row">
-          <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-            <div className="copyright-text">
-              <p>Copyright &copy; 2023, All Right Reserved 
-                <a href="#">Brokerax</a>
-              </p>
-            </div>
-          </div>
-          <div className="col-xl-2 col-lg-2 d-none d-lg-block text-right">
-            <div className="footer-menu">
-              <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Terms</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Policy</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="copyright-text">
+        <p>
+          Copyright &copy; 2023, All Right Reserved
+          <a href="/"> Brokerax</a>
+        </p>
       </div>
     </footer>
   );
